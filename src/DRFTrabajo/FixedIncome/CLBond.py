55f3a5dd9953e5fa314dbf29a508029fa4e7c9e6
gdelp <gd.31@outlook.com>
2023-12-19 14:41:43 -0300

    cambios a CLBond

--- src/DRFTrabajo/FixedIncome/CLBond.py
+++ src/DRFTrabajo/FixedIncome/CLBond.py
@@ -15,34 +15,31 @@ class CLBond:
     def set_tera(self):
         notional = 100
         start_date = self.coupons[0].startdate
-
-
         def objective_function(tera):
             return self.get_value(notional, tera[0], start_date) - notional
-
         initial_guess = 0.05
-
         tera = fsolve(objective_function, initial_guess)[0]
-
         return tera
-
 
     def get_day_count_fraction(self, start_date: date, end_date: date):
         ##actual/365
         days = (end_date - start_date).days
         return days / 365.0
 
-    def get_value(self, notional: float, rate: float, fecha: date):
+    def get_present_value(self, notional: float, rate: float, fecha: date):
         future_coupons = [coupon for coupon in self.coupons if coupon.coupondate >= fecha]
-
         if not future_coupons:
             raise ValueError("No future coupons available for present value calculation.")
-
         pv = 0
         for i, coupon in enumerate(future_coupons):
             day_count_fraction = self.get_day_count_fraction(fecha, coupon.coupondate)
             pv += (coupon.flow / (1 + rate ) **day_count_fraction)
         return pv*notional/100
+    
+    def get_par_value(self):
+        pass
+    def get_value(self):    
+        pass
 
     def calculate_duration(self, notional: float, discount_rate: float, initial_date: date):
         future_coupons = [coupon for coupon in self.coupons if coupon.coupondate >= initial_date]
@@ -51,7 +48,7 @@ class CLBond:
             raise ValueError("No future coupons available for duration calculation.")
 
         weighted_sum = 0
-        pv = self.get_value(notional, discount_rate, initial_date)
+        pv = self.get_present_value(notional, discount_rate, initial_date)
 
         for i, coupon in enumerate(future_coupons):
             day_count_fraction = self.get_day_count_fraction(initial_date, coupon.coupondate)
@@ -64,7 +61,7 @@ class CLBond:
     
     def get_dv01(self, notional: float, rate: float, fecha: date) ->float:
         duration = self.calculate_duration(notional, rate, fecha)
-        pv = self.get_value(notional, rate, fecha)
+        pv = self.get_present_value(notional, rate, fecha)
         dv01 = -pv*duration/10_000
         return dv01
         
