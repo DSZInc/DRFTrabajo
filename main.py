d8e99b1d4438c4da4f5a6ca6cb325d5c273342ec
gdelp <gd.31@outlook.com>
2023-12-19 16:13:21 -0300

    trabajo finalisimo finalizado

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-#from src.DRFTrabajo import FixedIncome
+## this is file just for testing
+
+from src.DRFTrabajo.FixedIncome import CLBond, FixedCoupon
+from datetime import date
 
 
-future_coupons = []
+coupon = FixedCoupon(100,6,0,date(2023,12,1),date(2024,12,1))
 
-if not future_coupons:
-    print("hola")
-else:
-    print("chao")
+coupons = [coupon]
+
+bono_zero_cupon = CLBond(coupons)
+
+value = bono_zero_cupon.get_value(1000000,5, date(2023,12,19))
+
+print(value)
+print(bono_zero_cupon.get_present_value(1000000,5, date(2023,12,19)))
--- src/DRFTrabajo/FixedIncome/CLBond.py
+++ src/DRFTrabajo/FixedIncome/CLBond.py
@@ -16,7 +16,7 @@ class CLBond:
         notional = 100
         start_date = self.coupons[0].startdate
         def objective_function(tera):
-            return self.get_value(notional, tera[0], start_date) - notional
+            return self.get_present_value(notional, tera[0], start_date) - notional
         initial_guess = 0.05
         tera = fsolve(objective_function, initial_guess)[0]
         return tera
@@ -39,7 +39,7 @@ class CLBond:
             day_count_fraction = self.get_day_count_fraction(fecha, coupon.coupondate)
             pv += (coupon.flow / (1 + rate ) **day_count_fraction)
         return pv*notional/100
-    
+        
     def get_par_value(self, fecha: date):
         future_coupons = self.get_future_coupons(fecha)
         acc_fecha_ini  = future_coupons[0].startdate
@@ -54,7 +54,8 @@ class CLBond:
         price = 100*(pv/valpar)
         price = round(price,4)
         amount_to_pay = (valpar*price) *(notional/10000)
-        return amount_to_pay
+        return amount_to_pay 
+
 
     def calculate_duration(self, notional: float, discount_rate: float, initial_date: date):
         future_coupons = self.get_future_coupons(fecha)
@@ -64,11 +65,8 @@ class CLBond:
         for i, coupon in enumerate(future_coupons):
             day_count_fraction = self.get_day_count_fraction(initial_date, coupon.coupondate)
             weighted_sum += day_count_fraction * (coupon.flow / (1 + discount_rate) ** day_count_fraction)/100 * notional
-
         duration = weighted_sum / pv    
-
         return duration
-
     
     def get_dv01(self, notional: float, rate: float, fecha: date) ->float:
         duration = self.calculate_duration(notional, rate, fecha)
